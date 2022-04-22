07366fd3f09a5ed8edefe7daf1428c819094b587
Abhinav Rishi <abhirishi0805@gmail.com>
2022-04-23 01:57:12 +0530

    investment animation added

--- app/src/main/java/com/example/finbuddy/Fragments/TaxFragment.java
+++ app/src/main/java/com/example/finbuddy/Fragments/TaxFragment.java
@@ -10,15 +10,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.finbuddy.R;
+import com.white.progressview.HorizontalProgressView;
 
 
 public class TaxFragment extends Fragment {
     TextView tax_info;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tax, container, false);
-        tax_info=view.findViewById(R.id.tax_info);
+
+        tax_info = view.findViewById(R.id.tax_info);
         tax_info.setText("Tax is the money paid to the government other than for transaction-specific goods and services. " +
                 "Income tax is the most common tax in which a part of your income is paid to government. ");
         return view;
