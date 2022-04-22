0fc72bfa11588829a0c29f51acb1afcba6d42157
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2022-04-22 18:41:43 +0530

    Created basic fragments

--- app/src/main/java/com/example/finbuddy/Activity/MainActivity.java
+++ app/src/main/java/com/example/finbuddy/Activity/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.finbuddy;
+package com.example.finbuddy.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+
+import com.example.finbuddy.Fragments.HomeScreen;
+import com.example.finbuddy.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,
+                new HomeScreen()).commit();
     }
 }
---
+++ app/src/main/java/com/example/finbuddy/Fragments/HomeScreen.java
@@ -0,0 +1,20 @@
+package com.example.finbuddy.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.finbuddy.R;
+
+public class HomeScreen extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
