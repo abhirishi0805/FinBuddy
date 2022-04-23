15fbdc744850bd21716146aaa534ef3219b142c7
Abhinav Rishi <abhirishi0805@gmail.com>
2022-04-23 08:20:38 +0530

    home screen ui

--- app/src/main/java/com/example/finbuddy/Fragments/HomeScreen.java
+++ app/src/main/java/com/example/finbuddy/Fragments/HomeScreen.java
@@ -9,6 +9,7 @@ import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -24,28 +25,28 @@ import java.util.ArrayList;
 
 public class HomeScreen extends Fragment {
 
-    RelativeLayout tax_fragment , investment_fragment;
+    CardView card_tax, card_investment, card_education_loan;
     ViewPager2 viewPager2;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
         getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         getActivity().getWindow().setStatusBarColor(ContextCompat.getColor(getContext(), R.color.background_white));
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        tax_fragment = view.findViewById(R.id.tax_fragment);
-        investment_fragment = view.findViewById(R.id.investment_fragment);
-        viewPager2 = view.findViewById(R.id.view_pager2);
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        card_tax = view.findViewById(R.id.card_tax);
+        card_investment = view.findViewById(R.id.card_investment);
+        card_education_loan = view.findViewById(R.id.card_education_loan);
+        viewPager2 = view.findViewById(R.id.view_pager2);
 
         NavController navController = Navigation.findNavController(view);
 
@@ -58,20 +59,25 @@ public class HomeScreen extends Fragment {
         sliderAdapter adapter  = new sliderAdapter(getContext(), arr , viewPager2 );
         viewPager2.setAdapter(adapter);
 
-        tax_fragment.setOnClickListener(new View.OnClickListener() {
+        card_tax.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 navController.navigate(R.id.action_homeScreen2_to_taxFragment2);
             }
         });
 
-        investment_fragment.setOnClickListener(new View.OnClickListener() {
+        card_investment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 navController.navigate(R.id.action_homeScreen2_to_investmentFragment2);
             }
         });
 
-
+        card_education_loan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_homeScreen2_to_investmentFragment2);
+            }
+        });
     }
 }
