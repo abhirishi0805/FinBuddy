77322e954476b7af205ecf750a4552dd8aaf0809
Abhinav Rishi <abhirishi0805@gmail.com>
2022-04-23 06:58:32 +0530

    splash screen initial

--- app/src/main/java/com/example/finbuddy/Activity/SplashActivity.java
+++ app/src/main/java/com/example/finbuddy/Activity/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.finbuddy.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import com.example.finbuddy.R;
 
@@ -12,5 +14,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new CountDownTimer(2700, 1000) {
+            public void onTick(long millisUntilFinished) { }
+
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }.start();
     }
 }
